53131b4d06d6c38d97aa726e18e9b48c8265bede
Lucasrrosa <l_r_rosa@hotmail.com>
2018-04-27 18:08:32 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css'
+import CardFilme from './components/CardFilme'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CardFilme
+        />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/CardFilme.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class CardFilme extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+CardFilme.propTypes = {
+    filme: PropTypes.object.isRequired
+}
+
+export default CardFilme
